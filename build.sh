b6461f7c9ce150650500610acb952c43a7b3ebda
zst123 <zst123@users.noreply.github.com>
2023-03-20 16:48:30 +0800

    Fix build with bibtex

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ mkdir -p output
 cd src
 
 # Build multiple times
-pdflatex --output-directory=../output paper.tex
+pdflatex --output-directory=../output paper.tex && \
+biber ../output/paper.bcf && \
 pdflatex --output-directory=../output paper.tex
 
